69cb1fdb584923c73ab4b92a9eb8aacffc0f898b
John Tsantilis <i.tsantilis@yahoo.com>
2017-05-25 13:31:07 +0300

    code refactoring

--- src/main/java/org/lumi/springbootloggingtest/controller/WelcomeController.java
+++ src/main/java/org/lumi/springbootloggingtest/controller/WelcomeController.java
@@ -1,17 +1,12 @@
 package org.lumi.springbootloggingtest.controller;
 
-import org.lumi.springbootloggingtest.model.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by John Tsantilis
@@ -33,18 +28,9 @@ public class WelcomeController {
 
     }
 
-    /*@RequestMapping(method= RequestMethod.GET)
-    public @ResponseBody
-    Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-
-    }*/
-
     // inject via application.properties
     @Value("${welcome.message:test}")
     private String message = "Hello World";
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
 }
